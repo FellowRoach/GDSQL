5ec991fb7048e3ece365b265a1b5947b4dc46508
jinyangcruise <jinyangcruise@gmail.com>
2026-05-27 10:09:26 +0800

    Remove useless test and demo

--- demo/gbatis/entity/guild_entity.gd
+++
@@ -1,5 +0,0 @@
-extends GDSQL.GBatisEntity
-class_name DemoGuildEntity
-var id: int
-var name: String
-var faction: String
--- demo/gbatis/entity/hero_entity.gd
+++
@@ -1,7 +0,0 @@
-extends GDSQL.GBatisEntity
-class_name DemoHeroEntity
-var id: int
-var name: String
-var hp: int
-var mp: int
-var class_type: String
